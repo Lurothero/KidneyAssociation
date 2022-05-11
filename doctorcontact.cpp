4e59f347243204a621a801e320a9654f83dd8c82
Lurothero <reneallen75@gmail.com>
2022-05-10 22:21:06 -0600

    removed refresh button in doctor contact

--- doctorcontact.cpp
+++ doctorcontact.cpp
@@ -9,7 +9,6 @@ DoctorContact::DoctorContact(QWidget *parent) :
     Database record;
     ui->setupUi(this);
     connect(this,SIGNAL(clicked()),this,SLOT(AddDoctorRecord()));
-    //connect(this,SIGNAL(clicked()),this,SLOT(refreshButton()));
     loadRecords();
     indexConversion();
 
@@ -120,18 +119,9 @@ void DoctorContact::on_DeleteRecord_BTN_clicked()
     tokenizeList.clear();
 
     //Reload records
+    ui->FirstName_LineEdit->clear();
+    ui->LastName_LineEdit->clear();
+    ui->Phone_LineEdit->clear();
+    ui->Email_LineEdit->clear();
      loadRecords();
 }
-
-//May become obsolete
-void DoctorContact::on_Refresh_BTN_clicked()
-{
-    DoctorContact * docForm = new DoctorContact();
-    this->close();
-    docForm->show();
-
-    qDebug() << "Refresh Button";
-
-    //int id = ui->AddRecord_BTN.;
-}
-
